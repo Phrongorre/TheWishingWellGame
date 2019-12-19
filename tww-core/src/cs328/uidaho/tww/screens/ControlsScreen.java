62ef7d9f49c4f79ab40f0e19405cf7443f1b7513
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2019-12-18 21:15:55 -0800

    Splash Screen and Font Updates

--- tww-core/src/cs328/uidaho/tww/screens/ControlsScreen.java
+++ tww-core/src/cs328/uidaho/tww/screens/ControlsScreen.java
@@ -1,20 +1,25 @@
 package cs328.uidaho.tww.screens;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputEvent.Type;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 import cs328.uidaho.tww.BaseGame;
+import cs328.uidaho.tww.GameMetaData;
+import cs328.uidaho.tww.actors.BaseActor;
 
-public class MenuScreen extends BaseScreen {
-	
+public class ControlsScreen extends BaseScreen {
+
 	@Override
 	public void initialize() {
+		//Zoom in by 2x
+		this.mainStage.getCamera().viewportWidth  /= 2f;
+		this.mainStage.getCamera().viewportHeight /= 2f;
+		(new BaseActor("gui/controls_splash.png", this.mainStage)).centerAtPosition(400f, 300f);;
 		
 		//Add Start Button
-		TextButton startButton = new TextButton("Start", BaseGame.textButtonStyle);
+		TextButton startButton = new TextButton("Click to Begin", BaseGame.textButtonStyle);
 		startButton.addListener(
 			(Event e) -> {
 				if (!(e instanceof InputEvent) ||
@@ -22,32 +27,19 @@ public class MenuScreen extends BaseScreen {
 					return false;
 				}
 				
-				BaseGame.setActiveScreen(new ControlsScreen());
-				return false;
-			}
-		);
-		
-		
-		//Add Quit Button
-		TextButton quitButton = new TextButton("Quit", BaseGame.textButtonStyle);
-		quitButton.addListener(
-			(Event e) -> {
-				if (!(e instanceof InputEvent) ||
-					!((InputEvent)e).getType().equals(Type.touchDown)) {
-					return false;
-				}
-				
-				Gdx.app.exit();
+				GameMetaData.setSpawnLocation(350f, 55f);
+				BaseGame.setActiveScreen(GameMetaData.getScreen(CloveHavenScreen.class));
 				return false;
 			}
 		);
 		
 		this.uiTable.pad(10f);
+		this.uiTable.add().expand(1, 1);
+		this.uiTable.row();
 		this.uiTable.add(startButton);
-		this.uiTable.add(quitButton);
 	}
 	
 	@Override
 	public void update(float dt) { }
-	
+
 }
